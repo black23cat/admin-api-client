87e02e8c2d5e4b582e05eb1187c1816ecdbdfef1
black23cat <fmother731@gmail.com>
2026-04-30 07:16:04 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+
+export default function App() {
+  const [heading, setHeading] = useState('Magnificent Monkeys');
+
+  const clickHandler = () => {
+    setHeading('Radical Rhinos');
+  };
+  return (
+    <>
+      <button type="button" onClick={clickHandler}>
+        Click Me
+      </button>
+      <h1>{heading}</h1>
+    </>
+  );
+}
