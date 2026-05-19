5e3dff71e81d09a597bde5a1d2c93d7a366e6a64
Prayogi Pangestu <fmother731@gmail.com>
2026-05-20 06:59:46 +0700

    chore: change filename pattern, add function to remove filename extension

--- src/utils/regexPattern.js
+++ src/utils/regexPattern.js
@@ -1,10 +1,11 @@
 export function matchFilename(filename) {
   const filenamePattern =
-    /^[a-zA-Z0-9\s-]+_\d{2}-\d{2}_\d{2,3}x\d{2,3}_(\d{1,3}x?|[aA][bB])$/;
+    /^[a-zA-Z0-9\s-]+_\d{2}-\d{2}_\d{2,3}x\d{2,3}_([a-zA-Z]+)_(\d{1,3}x?|[aA][bB])$/;
   return filenamePattern.test(filename);
 }
 
-export function matchFileExtension(filename) {
-  const fileFormatPattern = /\.tiff?$/i;
-  return fileFormatPattern.test(filename);
+export function removeExtension(filename) {
+  const extensionPattern = /\.[a-zA-Z0-9]+(?:\.[a-zA-Z0-9]+)*$/;
+
+  return filename.replace(extensionPattern, '');
 }
