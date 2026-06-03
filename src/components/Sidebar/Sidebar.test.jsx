a7b8274e3042385d80562ee3ae304256680d5fff
Prayogi Pangestu <fmother731@gmail.com>
2026-06-03 12:42:13 +0700

    test: Add test unit to check for button navigation

--- src/components/Sidebar/Sidebar.test.jsx
+++ src/components/Sidebar/Sidebar.test.jsx
@@ -1,7 +1,19 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, vi, beforeEach } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
 import Sidebar from './Sidebar';
+import userEvent from '@testing-library/user-event';
+
+let mockNavigate;
+
+vi.mock('react-router', async () => {
+  const actual = await vi.importActual('react-router');
+  return { ...actual, useNavigate: () => mockNavigate };
+});
+
+beforeEach(() => {
+  mockNavigate = vi.fn();
+});
 
 describe('Render Sidebar', () => {
   it('Render Sidebar correctly', () => {
@@ -20,5 +32,27 @@ describe('Render Sidebar', () => {
     expect(invoice).toBeInTheDocument();
     expect(jobData).toBeInTheDocument();
     expect(weeklyCash).toBeInTheDocument();
+  });
+});
+
+describe('Sidebar button redirect to correct route', () => {
+  it('Redirect to correct route', async () => {
+    const user = userEvent.setup({ pointerEventsCheck: 0 });
+
+    render(
+      <MemoryRouter>
+        <Sidebar />
+      </MemoryRouter>,
+    );
+
+    const purchaseOrderBtn = screen.getByRole('button', {
+      name: 'Puchase Order (PO)',
+    });
+    const invoice = screen.getByRole('button', { name: 'Invoice' });
+    await user.click(purchaseOrderBtn);
+    await user.click(invoice);
+    expect(mockNavigate).toHaveBeenCalledTimes(2);
+    expect(mockNavigate.mock.calls[0][0]).toEqual('/purchase-order');
+    expect(mockNavigate.mock.calls[1][0]).toEqual('/invoice');
   });
 });
