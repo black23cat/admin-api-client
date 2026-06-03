65781318d94577fbddd04b0aa3b383afbcbca563
Prayogi Pangestu <fmother731@gmail.com>
2026-06-03 12:47:58 +0700

    feat: Add navigate to button to redirect to specific route

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import toggleSidebarIcon from '../../images/toggle-sidebar.png';
+import { useNavigate } from 'react-router';
 
 export default function Sidebar() {
   const [showSidebar, setShowSidebar] = useState(false);
+  const navigate = useNavigate();
   const toggleSidebar = () => {
     setShowSidebar(!showSidebar);
   };
@@ -10,8 +12,18 @@ export default function Sidebar() {
     <aside className={`sidebar ${showSidebar ? 'show' : ''}`}>
       <div className="sidebar-menu">
         <p>Menu</p>
-        <button data-test-id="sidebar-button">Puchase Order (PO)</button>
-        <button data-test-id="sidebar-button">Invoice</button>
+        <button
+          data-test-id="sidebar-button"
+          onClick={() => navigate('/purchase-order')}
+        >
+          Puchase Order (PO)
+        </button>
+        <button
+          data-test-id="sidebar-button"
+          onClick={() => navigate('/invoice')}
+        >
+          Invoice
+        </button>
         <button data-test-id="sidebar-button">Data Kerja</button>
         <button data-test-id="sidebar-button">Kas Mingguan</button>
       </div>
