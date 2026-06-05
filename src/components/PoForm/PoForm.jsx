240099c0f12af1d84fe4e704897f624484685eec
Prayogi Pangestu <fmother731@gmail.com>
2026-06-05 07:29:51 +0700

    feat: Implement create new po Add customer name field Add poType select input Add button to delete valid uploaded file Set different error messages Change API URL path

--- src/components/PoForm/PoForm.jsx
+++ src/components/PoForm/PoForm.jsx
@@ -1,13 +1,25 @@
 import { useState } from 'react';
 import { matchFilename } from '../../utils/regexPattern';
+import { useNavigate } from 'react-router';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
 export default function PoForm() {
+  const [customerName, setCustomerName] = useState('');
+  const [poType, setPoType] = useState('eco');
   const [validFiles, setValidFiles] = useState([]);
   const [invalidFiles, setInvalidFiles] = useState([]);
-  const [error, setError] = useState(false);
   const [errorMsg, setErrorMsg] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleChange = (value) => {
+    setCustomerName(value);
+  };
+
+  const handlePoType = (value) => {
+    setPoType(value);
+  };
 
   const handleFiles = (e) => {
     const files = e.target.files;
@@ -27,31 +39,79 @@ export default function PoForm() {
     setValidFiles([...acceptedFiles, ...validFiles]);
   };
 
+  const deleteUploadedFile = (filename) => {
+    let firstMatch = false; //Only delete first match of file
+    const files = validFiles.filter((file) => {
+      if (file !== filename || firstMatch) {
+        return file;
+      }
+      firstMatch = true;
+    });
+    setValidFiles(files);
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (validFiles.length === 0) {
       return setErrorMsg('Tidak ada file yang diupload');
     }
+    if (customerName === '') {
+      return setErrorMsg('Nama Customer tidak boleh kosong');
+    }
+    const token = localStorage.getItem('token');
+    if (token === null) {
+      setErrorMsg(
+        'Token sudah expired, silahkan login ulang untuk membuat Invoice',
+      );
+    }
+    const fileList = validFiles.map((file) => {
+      return { filename: file };
+    });
     try {
-      const response = await fetch(`${API_URL}/api/order/create`, {
+      const response = await fetch(`${API_URL}/purchase-order`, {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(validFiles),
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          customerName,
+          poType,
+          fileList: fileList,
+        }),
       });
-      if (!response.status === 201) {
-        throw new Error('Fetch request failed');
+      if (response.status === 201) {
+        return navigate('/purchase-order');
       }
-    } catch (error) {
-      setError(true);
+      throw new Error('Gagal membuat invoice');
+    } catch {
+      setErrorMsg('Gagal membuat invoice');
     }
   };
   return (
     <>
-      {error && <span>Terjadi masalah ketika membuat Purchase Order(PO)</span>}
       <form onSubmit={handleSubmit}>
         <div>
           <label htmlFor="customer-name">Nama Customer :</label>
-          <input type="text" name="customer-name" id="customer-name" />
+          <input
+            type="text"
+            name="customer-name"
+            id="customer-name"
+            onChange={(e) => handleChange(e.target.value)}
+            value={customerName}
+          />
+        </div>
+        <div>
+          <label htmlFor="poType">Tipe po :</label>
+          <select
+            name="poType"
+            id="poType"
+            value={poType}
+            onChange={(e) => handlePoType(e.target.value)}
+          >
+            <option value="eco">Eco Solvent</option>
+            <option value="sublim">Sublim</option>
+          </select>
         </div>
         <div>
           <label htmlFor="files">Upload Files :</label>
@@ -67,7 +127,12 @@ export default function PoForm() {
           <p>Uploaded Files : </p>
           <ul>
             {validFiles.length > 0
-              ? validFiles.map((file, index) => <li key={index}>{file}</li>)
+              ? validFiles.map((file, index) => (
+                  <li key={index}>
+                    {file}
+                    <button onClick={() => deleteUploadedFile(file)}>x</button>
+                  </li>
+                ))
               : ''}
           </ul>
           <p>Invalid Files : </p>
