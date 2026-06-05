bd848f83b16e7c5c5b5d7a931f2727dade79bcfd
Prayogi Pangestu <fmother731@gmail.com>
2026-06-05 07:27:18 +0700

    feat: Add user context and lazy initialization on App render

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 import { createContext, useState } from 'react';
-import { Outlet, useNavigate } from 'react-router';
+import { Outlet } from 'react-router';
 import Header from './components/Header/Header';
 import Sidebar from './components/Sidebar/Sidebar';
 import LoginForm from './components/LoginForm/LoginForm';
-// import { jwtDecode } from 'jwt-decode';
+import { jwtDecode } from 'jwt-decode';
 
-// const currentTime = new Date();
+const currentTime = new Date();
+
+function init() {
+  // Check if user still have valid jwt on local storage
+  const token = localStorage.getItem('token');
+  if (token === null) {
+    return null;
+  }
+  const decodedToken = jwtDecode(token);
+  const tokenExpTime = new Date(decodedToken.exp * 1000); //jwt exp time must be times 1000 to get milisecond value
+  if (currentTime > tokenExpTime) {
+    return null;
+  }
+  return decodedToken.user;
+}
+
 export const UserContext = createContext({});
 
 export default function App() {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(init);
 
   if (user === null) {
     return (
