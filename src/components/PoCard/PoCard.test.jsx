e8486cc045d49c322891bb44dfa99792655d0027
Prayogi Pangestu <fmother731@gmail.com>
2026-06-11 18:33:50 +0700

    test: Change test on po Card Remove select po test Move button render test inside card click test Remove filelist test on card click

--- src/components/PoCard/PoCard.test.jsx
+++ src/components/PoCard/PoCard.test.jsx
@@ -3,7 +3,6 @@ import { render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
 import { format } from 'date-fns';
 import PoCard from './PoCard';
-import userEvent from '@testing-library/user-event';
 import { mockPoData } from '../../utils/mockData';
 
 const mockPurchaseOrder = mockPoData[0];
@@ -18,69 +17,34 @@ describe('Render Card', () => {
     );
     const customerName = screen.getByText('John Doe');
     const dateCreated = screen.getByText(`${currentDate}`);
-    const editButton = screen.getByAltText('edit purchase order');
-    const deleteButton = screen.getByAltText('delete purchase order');
-    const printPo = screen.getByAltText('print purchase order');
-    const selectPo = screen.getByTestId('select-button');
     expect(customerName).toBeInTheDocument();
     expect(dateCreated).toBeInTheDocument();
-    expect(editButton).toBeInTheDocument();
+  });
+
+  it("Shouldn't Render form, file list and action button when card isOpen=false", async () => {
+    render(
+      <MemoryRouter>
+        <PoCard purchaseOrder={mockPurchaseOrder} isOpen={false} />
+      </MemoryRouter>,
+    );
+
+    expect(() => screen.getByTestId('purchase-order-form')).toThrow();
+    expect(() => screen.getByAltText('delete purchase order')).toThrow();
+    expect(() => screen.getByAltText('print purchase order')).toThrow();
+  });
+
+  it('Render form, file list and action button when card isOpen=true', async () => {
+    render(
+      <MemoryRouter>
+        <PoCard purchaseOrder={mockPurchaseOrder} isOpen={true} />
+      </MemoryRouter>,
+    );
+    const poForm = screen.getByTestId('purchase-order-form');
+    const deleteButton = screen.getByAltText('delete purchase order');
+    const printButton = screen.getByAltText('print purchase order');
+
+    expect(poForm).toBeInTheDocument();
     expect(deleteButton).toBeInTheDocument();
-    expect(printPo).toBeInTheDocument();
-    expect(selectPo).toBeInTheDocument();
-  });
-
-  it('Select button is enabled when po is not invoiced', async () => {
-    render(
-      <MemoryRouter>
-        <PoCard purchaseOrder={mockPurchaseOrder} />
-      </MemoryRouter>,
-    );
-    const selectButton = screen.getByTestId('select-button');
-    expect(selectButton).not.toBeDisabled();
-  });
-
-  it('Select button is disabled when po is invoiced', async () => {
-    render(
-      <MemoryRouter>
-        <PoCard purchaseOrder={{ ...mockPurchaseOrder, invoiceId: 1 }} />
-      </MemoryRouter>,
-    );
-    const selectButton = screen.getByTestId('select-button');
-    expect(selectButton).toBeDisabled();
-  });
-
-  it('Render file list when card is active', async () => {
-    render(
-      <MemoryRouter>
-        <PoCard purchaseOrder={mockPurchaseOrder} expandCard={true} />
-      </MemoryRouter>,
-    );
-    const fileList = screen.getAllByText('Test print', { exact: false });
-    expect(fileList.length).toEqual(3);
-  });
-
-  it("Shouldn't Render file list when card is not active", async () => {
-    render(
-      <MemoryRouter>
-        <PoCard purchaseOrder={mockPurchaseOrder} />
-      </MemoryRouter>,
-    );
-    const fileList = screen.queryAllByText('Test print', { exact: false });
-    expect(fileList.length).toEqual(0);
-  });
-
-  it('Render po edit form when edit button is clicked', async () => {
-    const user = userEvent.setup({ pointerEventsCheck: 0 });
-    render(
-      <MemoryRouter>
-        <PoCard purchaseOrder={mockPurchaseOrder} expandCard={true} />
-      </MemoryRouter>,
-    );
-    const editButton = screen.getByAltText('edit purchase order');
-    await user.click(editButton);
-
-    const editPoForm = screen.getByTestId('edit-po-form');
-    expect(editPoForm).toBeInTheDocument();
+    expect(printButton).toBeInTheDocument();
   });
 });
