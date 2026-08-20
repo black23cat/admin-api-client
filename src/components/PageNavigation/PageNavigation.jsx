2412242319d31bdd976e8d920526bb2be84b9733
Prayogi Pangestu <fmother731@gmail.com>
2026-08-20 08:23:47 +0700

    style: Finish styling page navigation for mobile view

--- src/components/PageNavigation/PageNavigation.jsx
+++ src/components/PageNavigation/PageNavigation.jsx
@@ -1,3 +1,6 @@
+import Arrow from '../../assets/svg/Arrow';
+import styles from './PageNavigation.module.css';
+
 export default function PageNavigation({
   totalItemCount,
   currentPage,
@@ -21,25 +24,35 @@ export default function PageNavigation({
     handlePageNavigation(navigate);
   };
   return (
-    <nav className="page-navigation">
-      <button
-        type="button"
-        onClick={() => handlePageButtonNavigate('prev')}
-        disabled={currentPage === 1}
-      >
-        Prev
-      </button>
-      <span>{currentPage}</span>
-      <button
-        type="button"
-        onClick={() => handlePageButtonNavigate('next')}
-        disabled={isLastPage}
-      >
-        Next
-      </button>
-      <span>
-        Showing {showedItems} of {totalItemCount} Purchase Order Data
-      </span>
+    <nav className={styles['page-navigation']}>
+      {' '}
+      <p>
+        Showing <span>{showedItems}</span> of <span>{totalItemCount}</span> PO
+        Data
+      </p>
+      <div className={styles['button-wrapper']}>
+        <button
+          className={styles.prev}
+          type="button"
+          onClick={() => handlePageButtonNavigate('prev')}
+          disabled={currentPage === 1}
+        >
+          <Arrow />
+          <p>Prev</p>
+        </button>
+        <p>{currentPage}</p>
+        <button
+          className={styles.next}
+          type="button"
+          onClick={() => handlePageButtonNavigate('next')}
+          // disabled={isLastPage}
+        >
+          Next
+          <span>
+            <Arrow />
+          </span>
+        </button>
+      </div>
     </nav>
   );
 }
